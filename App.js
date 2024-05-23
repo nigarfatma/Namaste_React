d909de9bc133575b18d829fa0f09261d9dae9659
nigar_fatma <nigarfatma180@gmail.com>
2024-05-23 14:45:19 +0530

    add ep2 and ep3

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heaing = React.createElement(
 //   "h1",
 //   { id: "heading" },
@@ -40,17 +42,56 @@
 </div>
 </div> */
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am child2 h1"),
-    React.createElement("h1", {}, "I am child2 in h2"),
-  ])
-);
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am child2 h1 🚀"),
+//     React.createElement("h1", {}, "I am child2 in h2"),
+//   ])
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// laying the foundation
+const heading = React.createElement("h1", { id: "headig" }, "This is React");
+console.log("heading", heading);
+const jsxHeading = <h1>This is jsx heading</h1>;
+console.log("jsxHeading", jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+root.render(jsxHeading);
+
+// REACT COMPONENT-->there are two types of component-->1.class based componet(old way) 2.function component(new way)
+// React functional component
+const number = 1000;
+const elem = <span>JSX</span>;
+const Tiltle = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 id="heading">TITLLE OF RAECT TUORIAL {elem}</h1>
+      </div>
+    </>
+  );
+};
+const HeadigComponent = () => {
+  return (
+    <>
+      <div className="container">
+        {console.log({ Tiltle })}
+        {console.log("fghf")}
+        <h1 id="heading">RAECT TUORIAL</h1>
+        {/* same allof them */}
+        {/* <Tiltle /> */}
+        {/* <Tiltle></Tiltle> */}
+        {Tiltle()}
+      </div>
+    </>
+  );
+};
+root.render(<HeadigComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+Episode1-->
+browser basically knowns the js(like document,innerhtml) nut it does not known what is react so we just put react link in the file(using CDN)
 concept✍️
+Episode 2(Igniting(meaning-->fire) our app)-->
+1.React is not fast our applocation but lot of package is make fast
+2.NPM-->NODE PACKAGE MANAGER(no..npm is not node pacakge manager)-->npm does not have full form-->npm mange pacakge but it does not stand node pacakge manager
+3,what is npm ?
+ans:npm is a standard reposity for all the packages
+package.json is configration for npm
+4.what is buddler
+ans:eg:webpack,parcel,veet
+parcel--> npm install -D parcel --->-D meaning
+there are 2 types of depedieces-->1.dev dependiences(it requires in development) 2.normal depedieces(it rrquires in prodction) -->read more in both depndiences
+5.caret and tilde in package.json-->Read mmore
+6.difference between package.json and package-lock.json
+
+<!-- epi-03--lying the foundation -->
+
+jsx(transpiler before it reaches the js) -PARCEL - Babel
+jsx==>babel transpiler it to React.createElement ==>ReactElememt-->js object-->HTMLElemt(render)
+// REACT COMPONENT-->there are two types of component-->1.class based componet(old way) 2.functional component(new way)
+functional component:function component is a function that return a piece of code
+difference between element and component
+
+<!-- COMPONENT COMPOSITIONS -->
+
+episode-4:talk is cheap
